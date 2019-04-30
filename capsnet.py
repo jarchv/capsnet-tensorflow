77f7701e4252b20fb53d149e4884f014106cb3f8
joseucsp <josechavez.ie@gmail.com>
2019-04-30 16:28:40 -0500

    bug fixed for capsnet.py

--- capsnet.py
+++ capsnet.py
@@ -3,129 +3,124 @@ import tensorflow as tf
 from capsules import CapsNet as caps
 
 class CapsNet:
-	def __init__(self, mode = 'train',
-									classes = 10, 
-									m_plus  = 0.9,
-									m_minus = 0.1,
-									lambda_ = 0.5,   
-									alpha   = 0.0005,
-									rounds  = 3) :  				
+  def __init__(self, 
+                  mode = 'train',
+                  classes = 10, 
+                  m_plus  = 0.9,
+                  m_minus = 0.1,
+                  lambda_ = 0.5,   
+                  alpha   = 0.0005,
+                  rounds  = 3) :  				
 
-		self.X = tf.placeholder(shape = [None, 28, 28, 1], 
-														dtype = tf.float32, 
-														name  = 'X')
+    self.X = tf.placeholder(shape = [None, 28, 28, 1], 
+                            dtype = tf.float32, 
+                            name  = 'X')
 		
-		self.classes = classes
-		self.m_plus  = m_plus
-		self.m_minus = m_minus
-		self.lambda_ = lambda_
-		self.alpha   = alpha
-		self.rounds  = rounds
+    self.classes = classes
+    self.m_plus  = m_plus
+    self.m_minus = m_minus
+    self.lambda_ = lambda_
+    self.alpha   = alpha
+    self.rounds  = rounds
 
-		self.build_model()
+    self.build_model()
 
-	def build_model(self):
-
-		self.X_pad     = tf.image.resize_image_with_crop_or_pad(self.X, 32, 32)
-		self.X_cropped = tf.random_crop(self.X_pad, [tf.shape(self.X)[0], 28, 28, 1])
+  def build_model(self):
+    self.X_pad     = tf.image.resize_image_with_crop_or_pad(self.X, 32, 32)
+    self.X_cropped = tf.random_crop(self.X_pad, [tf.shape(self.X)[0], 28, 28, 1])
 		
-		self.Conv1 		 = caps('conv2d').layer(inputs = self.X_cropped,
-																					filters = 256, 
-																					kernel_size = 9, 
-																					activation = tf.nn.relu,
-																					name   = 'Conv1'
-																					)
+    self.Conv1 		 = caps('conv2d').layer(inputs      = self.X_cropped,
+                                          filters     = 256, 
+                                          kernel_size = 9, 
+                                          activation  = tf.nn.relu,
+                                          name        = 'Conv1'
+                                          )
 
-		self.PrimaryCaps = caps('primary').layer(inputs = self.Conv1,
-																						kernel_size = 9,
-																						strides 	= 2,
-																						activation  = tf.nn.relu,
-																						caps_units  = 32,
-																						caps_dim    = 8,
-																						name 		= 'PrimaryCaps'
-																						)
+    self.PrimaryCaps = caps('primary').layer(inputs     = self.Conv1,
+                                            kernel_size = 9,
+                                            strides     = 2,
+                                            activation  = tf.nn.relu,
+                                            caps_units  = 32,
+                                            caps_dim    = 8,
+                                            name        = 'PrimaryCaps'
+                                            )
 
-		self.DigitCaps 	= caps('digit').layer(inputs = self.PrimaryCaps,
-																				caps_units 	= self.classes,
-																				caps_dim  	= 16,
-																				rounds		= self.rounds,
-																				name		= 'DigitCaps'
-																				) # [?, 1, 10, 16, 1]
+    self.DigitCaps  = caps('digit').layer(inputs    = self.PrimaryCaps,
+                                        caps_units  = self.classes,
+                                        caps_dim    = 16,
+                                        rounds      = self.rounds,
+                                        name        = 'DigitCaps'
+                                        ) # [?, 1, 10, 16, 1]
 
-		self.y = tf.placeholder(shape = [None], dtype = tf.int64, name = 'y')
+    self.y = tf.placeholder(shape = [None], dtype = tf.int64, name = 'y')
 
-		with tf.variable_scope('masking'):
-			self.v_j_length = self.safe_length(self.DigitCaps, axis = -2) # [?, 1, 10, 1]
-			self.batch_loss = self.margin_loss() + self.reconstruction_loss() * self.alpha	
+    with tf.variable_scope('masking'):
+      self.v_j_length = self.safe_length(self.DigitCaps, axis = -2) # [?, 1, 10, 1]
+      self.batch_loss = self.margin_loss() + self.reconstruction_loss() * self.alpha	
 
-		with tf.variable_scope('Accuracy'):
-			self.correct  = tf.equal(self.y, self.y_pred, name = 'correct')
-			self.accuracy = tf.reduce_mean(tf.cast(self.correct, tf.float32), name = 'accuracy_mean')
+    with tf.variable_scope('Accuracy'):
+      self.correct  = tf.equal(self.y, self.y_pred, name = 'correct')
+      self.accuracy = tf.reduce_mean(tf.cast(self.correct, tf.float32), name = 'accuracy_mean')
 
-		with tf.variable_scope('Train'):
-			global_step = tf.Variable(0, trainable=False)
-			self.optimizer = tf.train.AdamOptimizer()
-			self.train_op  = self.optimizer.minimize(self.batch_loss, global_step=global_step, name = 'train_op')
+    with tf.variable_scope('Train'):
+      global_step = tf.Variable(0, trainable=False)
+      self.optimizer = tf.train.AdamOptimizer()
+      self.train_op  = self.optimizer.minimize(self.batch_loss, global_step=global_step, name = 'train_op')
 
-	def safe_length(self, v_j, axis, epsilon = 1e-9, keepdims = False, name = None):
-		with tf.variable_scope('safe_lenght'):
-			v_j_squared =  tf.reduce_sum(	tf.square(v_j), 
-											axis 	 = axis,
-											keepdims = keepdims)
-			return tf.sqrt(v_j_squared + epsilon, name = 'v_j_length')
+  def safe_length(self, v_j, axis, epsilon = 1e-9, keepdims = False, name = None):
+    with tf.variable_scope('safe_lenght'):
+      v_j_squared =  tf.reduce_sum(	tf.square(v_j), 
+                      axis      = axis,
+                      keepdims  = keepdims)
+      return tf.sqrt(v_j_squared + epsilon, name = 'v_j_length')
 
-	def margin_loss(self):
+  def margin_loss(self):
+    self.present_e_raw  = tf.square(tf.maximum(0.0, self.m_plus - self.v_j_length),  name = 'present_error_raw')
+    self.absent_e_raw 	= tf.square(tf.maximum(0.0, self.v_j_length - self.m_minus), name = 'absent_error_raw')
 
-		self.present_e_raw  = tf.square(tf.maximum(0.0, self.m_plus - self.v_j_length),  name = 'present_error_raw')
-		self.absent_e_raw 	= tf.square(tf.maximum(0.0, self.v_j_length - self.m_minus), name = 'absent_error_raw')
+    self.present_e = tf.reshape(self.present_e_raw, shape = (-1, self.classes))
+    self.absent_e  = tf.reshape(self.absent_e_raw , shape = (-1, self.classes))
 
-		self.present_e = tf.reshape(self.present_e_raw, shape = (-1, self.classes))
-		self.absent_e  = tf.reshape(self.absent_e_raw , shape = (-1, self.classes))
+    T = tf.one_hot(self.y, depth = self.classes, name = 'T')
+    L = tf.add(T * self.present_e, self.lambda_ * (1.0 - T) * self.absent_e) 
 
-		
-		T = tf.one_hot(self.y, depth = self.classes, name = 'T')
+    margin_loss = tf.reduce_sum(L, axis = 1, name = 'margin_loss')
+    batch_loss  = tf.reduce_mean(margin_loss, name = 'batch_loss')
 
+    return batch_loss
 
-		L = tf.add(T * self.present_e, self.lambda_ * (1.0 - T) * self.absent_e) 
+  def reconstruction_loss(self):
+    self.argmax_target = tf.argmax(self.v_j_length, axis = 2, name = 'argmax_target') # [?, 1, 1]
+    self.y_pred        = tf.squeeze(self.argmax_target, axis = [1, 2], name = 'y_pred') # [?,]
 
-		margin_loss = tf.reduce_sum(L, axis = 1, name = 'margin_loss')
-		
-		batch_loss  = tf.reduce_mean(margin_loss, name = 'batch_loss')
+    self.reconstruction = tf.placeholder_with_default(False, shape = (), name = 'label_mask')		
+    self.label_to_mask  = tf.cond(self.reconstruction, lambda: self.y, lambda: self.y_pred, name = 'label_to_mask')
 
-		return batch_loss
+    self.mask 				= tf.one_hot(self.label_to_mask, depth = self.classes, name = 'mask')
+    self.mask_reshaped  	= tf.reshape(self.mask, shape = [-1, 1, self.classes, 1, 1])
 
-	def reconstruction_loss(self):
-		self.argmax_target = tf.argmax(self.v_j_length, axis = 2, name = 'argmax_target') # [?, 1, 1]
-		self.y_pred        = tf.squeeze(self.argmax_target, axis = [1, 2], name = 'y_pred') # [?,]
+    self.caps_output_masked = tf.multiply(self.DigitCaps, self.mask_reshaped, name = 'caps_output_masked') # [?, 1, 16, 10, 1]
+    self.decoder_input = tf.reshape(self.caps_output_masked, shape = [-1, self.classes * self.DigitCaps.shape[-2].value]) # [?, 160]
 
-		self.reconstruction = tf.placeholder_with_default(False, shape = (), name = 'label_mask')		
-		self.label_to_mask  = tf.cond(self.reconstruction, lambda: self.y, lambda: self.y_pred, name = 'label_to_mask')
+    with tf.name_scope('Decoder'):
+      fc1 = tf.layers.dense(inputs     = self.decoder_input, 
+                            units      = 512,
+                            activation = tf.nn.relu,
+                            name       = 'fc1')
 
-		self.mask 				= tf.one_hot(self.label_to_mask, depth = self.classes, name = 'mask')
-		self.mask_reshaped  	= tf.reshape(self.mask, shape = [-1, 1, self.classes, 1, 1])
+      fc2 = tf.layers.dense(inputs      = fc1,
+                            units       = 1024,
+                            activation  = tf.nn.relu,
+                            name        = 'fc2')
 
-		self.caps_output_masked = tf.multiply(self.DigitCaps, self.mask_reshaped, name = 'caps_output_masked') # [?, 1, 16, 10, 1]
-		self.decoder_input = tf.reshape(self.caps_output_masked, shape = [-1, self.classes * self.DigitCaps.shape[-2].value]) # [?, 160]
+      self.decoder_output = tf.layers.dense(inputs      = fc2,
+													                  units       = 784,
+                                            activation  = tf.nn.sigmoid,
+                                            name        = 'decoder_output')
 
-		with tf.name_scope('Decoder'):
-			fc1 = tf.layers.dense(  inputs = self.decoder_input, 
-									units  = 512,
-									activation = tf.nn.relu,
-									name 	   = 'fc1')
+    self.flatten_X = tf.reshape(self.X_cropped, shape = [-1, 784], name = 'flatten_X')
 
-			fc2 = tf.layers.dense(	inputs = fc1,
-									units  = 1024,
-									activation  = tf.nn.relu,
-									name 		= 'fc2')
+    self.squared_diff = tf.square(self.flatten_X - self.decoder_output, name = 'squared_diff')
+    self.reconstruction_batch_loss = tf.reduce_mean(self.squared_diff, name = 'reconstruction_loss')
 
-			self.decoder_output = tf.layers.dense( 	inputs = fc2,
-													units  = 784,
-													activation  = tf.nn.sigmoid,
-													name  		= 'decoder_output')
-
-		self.flatten_X = tf.reshape(self.X_cropped, shape = [-1, 784], name = 'flatten_X')
-
-		self.squared_diff = tf.square(self.flatten_X - self.decoder_output, name = 'squared_diff')
-		self.reconstruction_batch_loss = tf.reduce_mean(self.squared_diff, name = 'reconstruction_loss')
-
-		return self.reconstruction_batch_loss
+    return self.reconstruction_batch_loss
